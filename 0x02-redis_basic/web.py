74a01519d482e661dffdd7b1b341024b1053247d
Christian Koranteng <korantengchristian@gmail.com>
2023-04-24 18:40:45 +0000

    Upload web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,82 +1,50 @@
 #!/usr/bin/env python3
 
-"""
-Web module for implementing an expiring web cache and tracker.
-
-This module defines a get_page function that uses the requests module to obtain
-the HTML content of a particular URL and caches the result with an expiration
-time of 10 seconds. The number of times a URL is accessed is also tracked.
-
-Functions:
-    get_page(url: str) -> str:
-        Returns the HTML content of the specified URL, either from the cache or
-        by fetching it using requests.get.
-
-Decorators:
-    cache_decorator(func: Callable[..., str]) -> Callable[..., str]:
-        A decorator that adds caching behavior to a function.
-
-Variables:
-    CACHE_EXPIRATION_TIME: int
-        The number of seconds until a cached result expires.
-"""
-
-import requests
 import redis
 import time
-from typing import Callable
-
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
-CACHE_EXPIRATION_TIME: int = 10
+import requests
+from typing import Optional
 
 
-def cache_decorator(func: Callable[..., str]) -> Callable[..., str]:
-    """
-    A decorator that adds caching behavior to a function.
+class RedisCache:
+    def __init__(self, host='localhost', port=6379, db=0, expire_time=10):
+        self.expire_time = expire_time
+        self.redis = redis.Redis(host=host, port=port, db=db)
 
-    Args:
-        func: A function that returns a string.
+    def get(self, key):
+        value = self.redis.get(key)
+        if value is not None:
+            return value.decode('utf-8')
+        return None
 
-    Returns:
-        A wrapper function that adds caching behavior to the original function.
-    """
-    def wrapper(url: str) -> str:
-        """
-        A wrapper function that adds caching behavior to the original function.
+    def set(self, key, value):
+        self.redis.setex(key, self.expire_time, value)
 
-        Args:
-            url: The URL to fetch.
+    def delete(self, key):
+        self.redis.delete(key)
 
-        Returns:
-            The HTML content of the specified URL, either from the cache or by
-            fetching it using requests.get.
-        """
-        cached_content = redis_client.get(url)
-        if cached_content is not None:
-            redis_client.incr(f"count:{url}")
-            return cached_content.decode('utf-8')
-        else:
-            content = func(url)
-            redis_client.set(url, content, ex=CACHE_EXPIRATION_TIME)
-            redis_client.set(f"count:{url}", 1, ex=CACHE_EXPIRATION_TIME)
+
+cache = RedisCache()
+
+
+def cache_decorator(func):
+    def wrapper(url: str) -> Optional[str]:
+        content = cache.get(url)
+        if content is not None:
+            # Use cached result if available
+            cache.redis.incr(f"count:{url}")
             return content
+
+        # Fetch new result and cache it
+        response = requests.get(url)
+        content = response.content.decode('utf-8')
+        cache.set(url, content)
+        cache.redis.setex(f"count:{url}", cache.expire_time, 1)
+        return content
 
     return wrapper
 
 
 @cache_decorator
-def get_page(url: str) -> str:
-    """
-    Returns the HTML content of the specified URL, either from the cache or by
-    fetching it using requests.get.
-
-    Args:
-        url: The URL to fetch.
-
-    Returns:
-        The HTML content of the specified URL, either from the cache or by
-        fetching it using requests.get.
-    """
-    response = requests.get(url)
-    content = response.content.decode('utf-8')
-    return content
+def get_page(url: str) -> Optional[str]:
+    return requests.get(url).text
