28f36f4078b7a0bf08e5fb531ca673cd8e8be2c1
Christian Koranteng <korantengchristian@gmail.com>
2023-04-25 16:54:51 +0000

    Upload 3 correction

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Lists all bands with Glam as their main style, ranked by their longevity
 
-SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
+SELECT band_name, COALESCE(split, 2022) - formed AS lifespan
 FROM metal_bands
 WHERE style like '%Glam rock%';
 
